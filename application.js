9a587c6e9a9a172a9f18f346ef7964ea332da4b7
MusikAnimal <ziemba0123@gmail.com>
2015-06-24 23:24:50 -0400

    handlebars for carousel, styling

--- application.js
+++ application.js
@@ -3093,7 +3093,24 @@ var __module0__ = (function(__dependency1__, __dependency2__, __dependency3__, _
 
 ;(function($) {
   app = Sammy('body', function() {
-    this.get('/');
+    this.get('/', function() {
+      var images = [
+        "Boat", "Macaw5", "Monkey2", "Jaguar", "3 toed Sloth"
+      ];
+
+      for(i in images) {
+        var index = parseInt(i);
+        var $html = $(Handlebars.templates.slide({
+          id: index,
+          image: images[i],
+          prevId: index-1 < 1 ? images.length-1 : index-1,
+          nextId: index+1 > images.length-1 ? 1 : index+1,
+          checkedText: i==0 ? "checked" : ""
+        }));
+        $(".carousel").append($html);
+      }
+    });
+
     this.get('itenineraries');
     this.get('pictures');
     this.get('contact');
@@ -3107,6 +3124,24 @@ var __module0__ = (function(__dependency1__, __dependency2__, __dependency3__, _
     app.addListeners();
   });
 })(jQuery);
+
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['slide'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<input id=\"img-"
+    + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
+    + "\" type=\"radio\" name=\"radio-btn\" "
+    + escapeExpression(((helper = (helper = helpers.checkedText || (depth0 != null ? depth0.checkedText : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"checkedText","hash":{},"data":data}) : helper)))
+    + " />\n<span class=\"slide-container\">\n  <div class=\"slide\">\n    <img src=\"images/"
+    + escapeExpression(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"image","hash":{},"data":data}) : helper)))
+    + ".jpg\" />\n  </div>\n  <div class=\"nav\">\n    <label class=\"prev\" for=\"img-"
+    + escapeExpression(((helper = (helper = helpers.prevId || (depth0 != null ? depth0.prevId : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"prevId","hash":{},"data":data}) : helper)))
+    + "\">&#x2039;</label>\n    <label class=\"next\" for=\"img-"
+    + escapeExpression(((helper = (helper = helpers.nextId || (depth0 != null ? depth0.nextId : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"nextId","hash":{},"data":data}) : helper)))
+    + "\">&#x203a;</label>\n  </div>\n</span>";
+},"useData":true});
+})();
 
 
 
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,23 @@
 ;(function($) {
   app = Sammy('body', function() {
-    this.get('/');
+    this.get('/', function() {
+      var images = [
+        "Boat", "Macaw5", "Monkey2", "Jaguar", "3 toed Sloth"
+      ];
+
+      for(i in images) {
+        var index = parseInt(i);
+        var $html = $(Handlebars.templates.slide({
+          id: index,
+          image: images[i],
+          prevId: index-1 < 1 ? images.length-1 : index-1,
+          nextId: index+1 > images.length-1 ? 1 : index+1,
+          checkedText: i==0 ? "checked" : ""
+        }));
+        $(".carousel").append($html);
+      }
+    });
+
     this.get('itenineraries');
     this.get('pictures');
     this.get('contact');
